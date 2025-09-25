57dfab1ea4c9757798f32a054232c7f73ae84892
maria-DX <mariaeduarda202901@gmail.com>
2025-09-24 22:37:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from times import Times
+
+def main():
+    t1 = Times()
+    t1.time_serie_a = 'Sport Clube Recife'
+    t1.time_serie_d = 'Santa Cruz'
+
+    t1.exibir_time_serie_a()
+    t1.exibir_time_serie_d()
+
+if __name__ == "__main__":
+    main()
---
+++ times.py
@@ -0,0 +1,7 @@
+class Times:
+    def exibir_time_serie_a(self):
+        print(f'Time série A: {self.time_serie_a}')
+
+    def exibir_time_serie_d(self):
+        print(f"Time série D: {self.time_serie_d}")
+        
